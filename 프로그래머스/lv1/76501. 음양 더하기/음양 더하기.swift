cf0e31695d5f125a8d1adcea240b35c92a110d37
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-07 22:03:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.03 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var result = 0
+    for i in 0 ..< signs.count {
+        if signs[i] {
+            result += absolutes[i]
+        }else{
+            result -= absolutes[i]
+        }
+    }
+    return result
+}
