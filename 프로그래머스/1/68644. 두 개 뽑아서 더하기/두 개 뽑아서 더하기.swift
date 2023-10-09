5d35eba4e704dd2a255294004732be6412e86b44
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-09 17:21:31 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.41 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result : Set<Int> = []
+    for i in 0 ..< numbers.count {
+        for j in i+1 ..< numbers.count {
+            result.insert(numbers[i]+numbers[j])
+        }
+    }
+    return result.sorted(by: <)
+}
