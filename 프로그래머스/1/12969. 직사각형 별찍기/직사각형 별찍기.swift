47420956c8b25dc06ec7bcd34c158ab7235ad4de
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-18 21:44:07 +0900

    [level 1] Title: 직사각형 별찍기, Time: 23.20 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -3,11 +3,9 @@ import Foundation
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 let (a, b) = (n[0], n[1])
 
-for i in 0 ..< b {
-    var str = ""
-    for j in 0 ..< a {
-        str.append("*")
+for _ in 1...b {
+    for _ in 1...a {
+        print("*", terminator: "")
     }
-    print(str)
+    print("")
 }
-
