190e1b45d4187c50e913b19dafd3ad6ee22e6a05
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-01-09 08:31:16 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, letter) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string[i] !== letter) {
+            
+            
+            answer = answer.concat(my_string[i]);
+        }
+    }
+    
+    return answer;
+}
