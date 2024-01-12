e1f7b356c74e3f4a25e7987d9c27c0c96df6ca7e
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-01-12 13:47:48 +0900

    [level Lv.0] Title: 콜라츠 수열 만들기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let result = []
+    result.push(n)
+    while(n != 1) {
+        
+        if(n % 2 === 0) {
+            n /= 2
+        } else {
+            n = 3 * n + 1
+        }
+        result.push(n)
+    }
+    return result
+}
