4b0ad3e9964aa209453a2d98e792a471a80d9c95
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-01-22 21:03:29 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,7 +1,6 @@
 function solution(num_list, n) {
     let result = []
-    for(let i=0;i<num_list.length; i+=n) {
-        result.push(num_list[i])
-    }
-    return result
+    return num_list.filter((_,i) => {
+        return i%n === 0
+    })
 }
