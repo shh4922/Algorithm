186b2ed5a3755a4c99abac5b6f626b9c699dd6f0
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-02-04 20:30:49 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 3.67 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    return arr.map((value) => {
+        if(value >= 50 && value%2 ===0) {
+            return (value/2)
+        } else if (value < 50 && value%2 !==0) {
+            return (value * 2)
+        }
+        return value    
+        
+    })
+    
+}
