32c2dc6bd6dd342ab9a461cb0fac5c67f3bd7502
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-04-11 09:14:04 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+if(!Number(input[1])||!Number(input[0])) {
+    console.log(0)
+} else {
+    console.log(Number(input[0])*Number(input[1]))
+}
