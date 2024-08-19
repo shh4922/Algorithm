2b93985e8375d5287c6ec530efdb3909267a156b
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-08-19 23:04:14 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.09 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,13 @@
+function solution(arr1, arr2) {
+    let result = []
+    for(let i=0; i<arr1.length; i++) {
+        let result2 = []
+        
+        for(let j=0; j<arr1[i].length; j++) {
+            
+            result2.push(arr1[i][j]+arr2[i][j])
+        }
+        result.push(result2)
+    }
+    return result
+}
