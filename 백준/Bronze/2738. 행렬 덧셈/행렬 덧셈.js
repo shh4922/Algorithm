5118ac4edeb34df250c588f6d87ad783581c2ef4
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-20 17:20:01 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 100 ms, Memory: 10648 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -1,30 +1,31 @@
-const input = require("fs")
-  .readFileSync("dev/stdin")
-  .toString().trim()
-  .split("\n");
-const [hang, yul] = input
-  .shift()
-  .split(" ")
-  .map((i) => +i);
-const a = [];
-const result = [];
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
 
-input.forEach((line) => {
-  const lineArr = line.split(" ").map((i) => +i);
-  a.push(lineArr);
-});
 
-const b = a.splice(hang, hang);
+const input = fs.readFileSync(filePath).toString().trim().split("\n")
 
-for (let i = 0; i < hang; i++) {
-  const smallA = a[i];
-  const smallB = b[i];
-  const arr = [];
-  for (let j = 0; j < yul; j++) {
-    const f = smallA[j] + smallB[j];
-    arr.push(f);
-  }
-  result.push(arr);
+const inputLine = []
+for (let i = 1; i < input.length; i++) {
+    inputLine.push(input[i].toString().trim().split(" ").map(Number))
 }
 
-result.map((i) => console.log(i.join(" ")));
+const size = input[0].toString().trim().split(" ").map(Number)
+
+const N = Number(size[0])
+const M = Number(size[1])
+
+const result = []
+
+for (let i = 0; i < N; i++) {
+    const ary = []
+    for (let j = 0; j < M; j++) {
+        const sum = inputLine[0 + i][j] + inputLine[N + i][j]
+        ary.push(sum)
+    }
+    result.push(ary)
+}
+
+result.forEach((row) => {
+    console.log(row.join(" "))
+})
+
