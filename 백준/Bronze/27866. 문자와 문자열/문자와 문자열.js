15af2699d50a09609c7204b95ee1ceb2a9e1d089
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-23 22:14:45 +0900

    [Bronze V] Title: 문자와 문자열, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split('\n')
+
+console.log(input[0][input[1]-1])
+
