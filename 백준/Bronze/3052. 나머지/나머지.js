a4e271d5b55a64b5d35f728884967600b0845c9e
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-23 22:16:16 +0900

    [Bronze II] Title: 나머지, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const obj = {}
+input.forEach((value) => {
+    obj[value % 42] = 1
+})
+console.log(Object.keys(obj).length)
