bf9b2362e3616fbc434088b5cb5d274aecaeb0ad
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-27 20:50:13 +0900

    [Bronze II] Title: 벌집, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.js
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -7,6 +7,10 @@ const input = Number(fs.readFileSync(filePath).toString().trim())
 let number = input
 let cnt = 1
 
+// 규칙을 보면 6, 12, 18 ... 칸수가 올라갈때마다 6의배수만큼 개수가 증가.
+// 1보다크면 계속해서 6, 12, 18 씩 뻄. 즉 한칸씩 이동한다는거
+// break가 걸리는 시점은 1이거나 1보다 작을때임
+
 while(number > 1) {    
     number -= (cnt*6)
     cnt += 1
