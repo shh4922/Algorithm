13bdf07585e3978c8042fed2a24edccc0d9d50ca
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-10-28 22:47:13 +0900

    [Bronze V] Title: 직사각형, Time: 92 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number)
+
+console.log(input[0]*input[1])
