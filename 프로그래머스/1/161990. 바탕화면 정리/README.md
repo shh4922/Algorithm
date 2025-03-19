5fec018ab4d3ee9f44718d7082dc3a974da7e654
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-19 23:40:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.59 ms
+메모리: 33.5 MB, 시간: 0.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 00:18:48
+2025년 03월 19일 23:40:32
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,26 +1,49 @@
+/**
+wallpaper는 스트링배열임.
+wallpaper의 길이가 y축의 길이가되고
+스트링의 길이가 x축 길이임
+낚이면 안되는부분은 파일시작은 왼쪽위 인데, 끝나는부분은 오른쪽아래여서 젤마지막파일은 x,y +1해야함
+*/
 function solution(wallpaper) {
-    // index = y
     const xList = []
     const yList = []
-    wallpaper.forEach((items, y)=> {
-        
-        for(let i=0; i<items.length; i++) {
-            if(items[i] === '#') {
-                yList.push(y)
-                xList.push(i)
-            }
-        }
-        
-    })
-    
-    const xMax = Math.max(...xList)
-    const xMin = Math.min(...xList)
-    const yMax = Math.max(...yList)
-    const yMin = Math.min(...yList)
     const result = []
-    result.push(yMin)
-    result.push(xMin)
-    result.push(yMax+1)
-    result.push(xMax+1)
+    wallpaper.forEach((fileList,y)=> {
+        for(let x=0; x<fileList.length; x++) {
+           if(fileList[x] === '#') {
+               xList.push(x)
+               yList.push(y) 
+           }
+        }
+    })
+    result.push(Math.min(...yList))
+    result.push(Math.min(...xList))
+    result.push(Math.max(...yList)+1)
+    result.push(Math.max(...xList)+1)
+    
     return result
+    // index = y
+//     const xList = []
+//     const yList = []
+//     wallpaper.forEach((items, y)=> {
+        
+//         for(let i=0; i<items.length; i++) {
+//             if(items[i] === '#') {
+//                 yList.push(y)
+//                 xList.push(i)
+//             }
+//         }
+        
+//     })
+    
+//     const xMax = Math.max(...xList)
+//     const xMin = Math.min(...xList)
+//     const yMax = Math.max(...yList)
+//     const yMin = Math.min(...yList)
+//     const result = []
+//     result.push(yMin)
+//     result.push(xMin)
+//     result.push(yMax+1)
+//     result.push(xMax+1)
+//     return result
 }
