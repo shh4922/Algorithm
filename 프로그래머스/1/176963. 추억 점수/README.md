378b015fcbab001bfa75682fe1a24ed162f425c0
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-19 23:23:32 +0900

    [level 1] Title: 추억 점수, Time: 22.94 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/README.md
+++ 프로그래머스/1/176963. 추억 점수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 추억 점수 - 176963 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 36.6 MB, 시간: 22.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 11:30:23
+2025년 03월 19일 23:23:30
 
 ### 문제 설명
 
--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,19 +1,35 @@
 function solution(name, yearning, photo) {
-    let dic = {}
-    for(let i = 0; i<name.length; i++) {
-        dic[name[i]] = yearning[i]
-    }
+//     let dic = {}
+//     for(let i = 0; i<name.length; i++) {
+//         dic[name[i]] = yearning[i]
+//     }
     
-   return  photo.map((ary)=> {
-        let sum = 0
-        for(let i=0; i<ary.length; i++) {
-            if(dic[ary[i]] !== undefined) {
-                sum += dic[ary[i]]     
-            }
+//    return  photo.map((ary)=> {
+//         let sum = 0
+//         for(let i=0; i<ary.length; i++) {
+//             if(dic[ary[i]] !== undefined) {
+//                 sum += dic[ary[i]]     
+//             }
             
             
-        }
-        return sum
+//         }
+//         return sum
+//     })
+    
+    const namePoint = {}
+    let sum = 0
+    const result = []
+    name.forEach((userName, index)=> {
+        namePoint[userName] = yearning[index]
     })
     
+    photo.forEach((userList)=> {
+        userList.forEach((user)=>{
+            if(namePoint[user] === undefined) { return }
+            sum+= namePoint[user]
+        })
+        result.push(sum)
+        sum = 0
+    })
+    return result
 }
