cf274c142ef5eed992ddb99a330d6642e43207df
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-19 23:18:09 +0900

    [level 1] Title: 달리기 경주, Time: 154.06 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.23 ms
+메모리: 84.1 MB, 시간: 154.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 11:14:24
+2025년 03월 19일 23:18:05
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,45 @@
 function solution(players, callings) {
-    let result = {}
-    for(let i = 0; i<players.length; i++) {
-        result[players[i]] = i
-    }
+    const dummyUsers = {}
+    players.forEach((player,index)=> {
+        dummyUsers[player] = index
+    })
     
-    
-    for(let i=0; i<callings.length; i++) {
-        const value = result[callings[i]]
-        const temp = players[value-1]
-              
-        players[value -1] = callings[i]
-        players[value] = temp
+    /**
+    이름 불리면, 불린애앞에 애의 등수 확인
+    앞애애 등수를 불린애 등수로 변경
+    불린애 등수를 앞에있던애의 등수로 변경
+    */
+    const copyPlayers = players
+    callings.forEach((callName, index)=> {
+        const prevRank = dummyUsers[callName] // 불린애 등수
+        const downGrageUser = copyPlayers[prevRank-1] // 앞에있던애의 이름
         
-        result[callings[i]] = value - 1
-        result[temp] = value
-    }
-    return players
+        // 결과값 변경
+        copyPlayers[prevRank-1] = callName
+        copyPlayers[prevRank] = downGrageUser
+        
+        // dummyObject value 변경
+        dummyUsers[callName] = prevRank-1
+        dummyUsers[downGrageUser] = prevRank
+    })
+    return copyPlayers
+
+    
+//     let result = {}
+//     for(let i = 0; i<players.length; i++) {
+//         result[players[i]] = i
+//     }
+    
+    
+//     for(let i=0; i<callings.length; i++) {
+//         const value = result[callings[i]]
+//         const temp = players[value-1]
+              
+//         players[value -1] = callings[i]
+//         players[value] = temp
+        
+//         result[callings[i]] = value - 1
+//         result[temp] = value
+//     }
+//     return players
 }
