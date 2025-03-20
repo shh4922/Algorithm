1577af8215f9b2bf4bdcd742bd6d2106ed5f9c9a
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-20 22:49:37 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,29 +1,57 @@
 function solution(keymap, targets) {
-    const dic = {}
-    for(let i=0; i<keymap.length; i++) {
-        for(let j = 0; j<keymap[i].length; j++) {
-            if(!dic[keymap[i][j]]) {
-                dic[keymap[i][j]] = j+1     
-            } else {
-                if(dic[keymap[i][j]] > j+1) {
-                    dic[keymap[i][j]] = j+1    
-                }
+    
+    const keyObject = {}
+    
+    keymap.forEach((keyList) => {
+        for(let i=0; i<keyList.length; i++) {
+            if(keyObject[keyList[i]] === undefined || keyObject[keyList[i]] > i) {
+                keyObject[keyList[i]] = i
             }
         }
-    }
-    let ary = []
-    for(let i=0; i<targets.length; i++) {
-        let result = 0
-        for(let j=0; j<targets[i].length; j++) {
-            if(dic[targets[i][j]] ) {
-                result += dic[targets[i][j]]
-            } else {
-                result = -1
+    })
+    
+    const result = []
+    
+    targets.forEach((keyList)=> {
+        let sum = 0
+        for(let i=0; i<keyList.length; i++) {
+            if(keyObject[keyList[i]] === undefined) {
+                result.push(-1)
                 break
             }
+            sum+= keyObject[keyList[i]]+1
+            if(i === keyList.length-1) {
+                result.push(sum)
+            }
         }
-        ary.push(result)
-    }
-    return ary
-    
+    })
+    return result
 }
+    
+    // const dic = {}
+    // for(let i=0; i<keymap.length; i++) {
+    //     for(let j = 0; j<keymap[i].length; j++) {
+    //         if(!dic[keymap[i][j]]) {
+    //             dic[keymap[i][j]] = j+1     
+    //         } else {
+    //             if(dic[keymap[i][j]] > j+1) {
+    //                 dic[keymap[i][j]] = j+1    
+    //             }
+    //         }
+    //     }
+    // }
+    // let ary = []
+    // for(let i=0; i<targets.length; i++) {
+    //     let result = 0
+    //     for(let j=0; j<targets[i].length; j++) {
+    //         if(dic[targets[i][j]] ) {
+    //             result += dic[targets[i][j]]
+    //         } else {
+    //             result = -1
+    //             break
+    //         }
+    //     }
+    //     ary.push(result)
+    // }
+    // return ary
+    
