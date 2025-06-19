484b2ccedf018368e6f9a4392ad310f46cf9db0f
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-06-20 00:31:38 +0900

    [level 1] Title: 달리기 경주, Time: 145.10 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -11,35 +11,20 @@ function solution(players, callings) {
     */
     const copyPlayers = players
     callings.forEach((callName, index)=> {
-        const prevRank = dummyUsers[callName] // 불린애 등수
-        const downGrageUser = copyPlayers[prevRank-1] // 앞에있던애의 이름
         
-        // 결과값 변경
-        copyPlayers[prevRank-1] = callName
-        copyPlayers[prevRank] = downGrageUser
+        const prevRank = dummyUsers[callName] // 기존 나의 랭크
+        const frontUserName = copyPlayers[prevRank-1] // 나보다 앞에있는애의 이름
         
-        // dummyObject value 변경
-        dummyUsers[callName] = prevRank-1
-        dummyUsers[downGrageUser] = prevRank
+        // 더미유저의 순서 변경
+        dummyUsers[callName] = prevRank-1 // 내꺼 랭크 올림
+        dummyUsers[frontUserName] = prevRank // 나보다 앞애있는애를 기존나의 랭크로 보냄
+        
+        // 등수체크할 유저의 위치 변경
+        copyPlayers[prevRank] = frontUserName // 기존 내 위치에있던 배열에 내앞에 애 이름 넣음
+        copyPlayers[prevRank-1] = callName // 내앞에 있던애 위치에 내 이름 넣음
+        
     })
     return copyPlayers
 
-    
-//     let result = {}
-//     for(let i = 0; i<players.length; i++) {
-//         result[players[i]] = i
-//     }
-    
-    
-//     for(let i=0; i<callings.length; i++) {
-//         const value = result[callings[i]]
-//         const temp = players[value-1]
-              
-//         players[value -1] = callings[i]
-//         players[value] = temp
-        
-//         result[callings[i]] = value - 1
-//         result[temp] = value
-//     }
-//     return players
+
 }
