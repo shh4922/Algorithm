cfd16c836cd0dfd8860fd44aa485599fcb5ea5d1
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-06-28 20:13:09 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.40 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,37 +1,83 @@
 function solution(board, moves) {
-    const box = []
-    let result = 0    
+    const stack = []
+    let result = 0
+    const copyBoard = board
     
-    for(let i=0; i<moves.length; i++) {
-        const line = moves[i]
+    // 4, 3, 1, 1, 3, 2, 4
+    moves.forEach((value) => {
         
-        for(let j=0; j<board.length; j++) {
-            //뽑을 위치
-            const selectedItem = board[j][line-1]
-            
-            // 뽑을 위치에 아이탬이있을시
-            if(selectedItem != 0) {
-                if(box[box.length-1] === selectedItem) {
-                    box.pop()
-                    result += 2
-                } else {
-                    box.push(selectedItem)    
+        for(let i=0; i<copyBoard.length; i++) {
+            if(copyBoard[i][value-1] !== 0) {
+                stack.push(copyBoard[i][value-1])
+                
+                // console.log(value, copyBoard[i][value-1])
+                copyBoard[i][value-1] = 0
+                if(checkPop(stack)) {
+                    result +=2
+                    stack.pop()
+                    stack.pop()
                 }
-                board[j][line-1] = 0
                 break
             }
+            
+            
         }
-    }
-    
-    
-    // for(let i = 0; i<box.length-1; i++) {
-    //     if (box[i] === box[i+1]) {
-    //         box.splice(i, 2)
-    //         result += 2
-    //         i = 0
-    //     }
-    // }
-    
-    return result;
+//         board.forEach((column)=> {
+//             if(column[value-1] !== 0) {
+//                 stack.push(column[value-1])
+//                 column[value-1] = 0
+                
+//                 if(checkPop(stack)) {
+                  
+//                 }
+//             }
+//         })
+    })
+    return result
 }
+
+function checkPop(stack) {
+    const length = stack.length-1
+    
+    const cur = stack[length]
+    const prev = stack[length-1]
+    
+    if(cur === prev) {
+        // console.log(cur,prev)
+        return true
+    }
+    return false
+}
+
+
+
+// function solution(board, moves) {
+//     const box = []
+//     let result = 0    
+    
+//     for(let i=0; i<moves.length; i++) {
+//         const line = moves[i]
+        
+//         for(let j=0; j<board.length; j++) {
+//             //뽑을 위치
+//             const selectedItem = board[j][line-1]
+            
+//             // 뽑을 위치에 아이탬이있을시
+//             if(selectedItem != 0) {
+                
+//                 // 0개일떄 또는 같은거없을땐 else로 이동
+//                 if(box[box.length-1] === selectedItem) {
+//                     box.pop()
+//                     result += 2
+//                 } else {
+//                     box.push(selectedItem)    
+//                 }
+//                 board[j][line-1] = 0
+//                 break
+//             }
+//         }
+//     }
+    
+    // return result;
+// }
 
