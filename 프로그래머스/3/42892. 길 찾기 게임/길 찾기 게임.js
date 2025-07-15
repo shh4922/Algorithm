6d88e12b12008e94135f5e1a4850d7ae6b0aa96b
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-07-15 20:40:42 +0900

    [level 3] Title: 길 찾기 게임, Time: 77.09 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
@@ -16,37 +16,18 @@ class Node {
 }
 
 // 결국 좌측 하위의 하위 노드는, 최상위의 x보다는 작거나 같을수밖에 없음
+// 우측의 하위의 하위노드는, 최상위 노드의 x보다는 클수밖에없음
 function solution(nodeinfo) {
     const newNodeInfo = nodeinfo
         .map((value,index) => {return [...value, index+1]})
         .sort((a,b)=> { return b[1] - a[1] })
     
-
     
-    const map = new Map()
-    newNodeInfo.forEach((value)=> {
-        const node = new Node({
-            x: value[0],
-            y: value[1],
-            value: value[2]                  
-        })
-        if(map.get(value[1])) {
-            map.set(value[1], [...map.get(value[1]), node])
-        } else {
-            map.set(value[1], [node])
-        }
-    })
-    
-    let nodeList = []
-    Array.from(map).forEach(([key,value])=> {
-        value.sort((a,b)=> {
-            return a.data.x - b.data.x
-        })
-        nodeList = [...nodeList, ... value]
-    })
-    console.log(nodeList)
+    const map = makeTree(newNodeInfo)
+    let nodeList = makeSortedTreeList(map)
     
     let root = null
+    
     for(const node of nodeList) {
         if(root === null) {
             root = node
@@ -62,7 +43,7 @@ function solution(nodeinfo) {
                     }
                     
                 }
-                else {                                  // 부모 오른쪽이 없는데, 내 x좌표가 부모보다 큰경우
+                else {                                  // 내 x좌표가 부모보다 큰경우
                     if(parent.isRight()) {
                         parent = parent.right
                     } else {
@@ -78,6 +59,34 @@ function solution(nodeinfo) {
     result.push(postOrder(root, []))
     return result
     
+}
+
+const makeTree = (newNodeInfo) => {
+    const map = new Map()
+    newNodeInfo.forEach((value)=> {
+        const node = new Node({
+            x: value[0],
+            y: value[1],
+            value: value[2]                  
+        })
+        if(map.get(value[1])) {
+            map.set(value[1], [...map.get(value[1]), node])
+        } else {
+            map.set(value[1], [node])
+        }
+    })
+    return map
+}
+
+const makeSortedTreeList = (map) => {
+    let nodeList = []
+    Array.from(map).forEach(([key,value])=> {
+        value.sort((a,b)=> {
+            return a.data.x - b.data.x
+        })
+        nodeList = [...nodeList, ... value]
+    })
+    return nodeList
 }
 
 const preOrder = (node, result) => {
