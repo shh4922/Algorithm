b3408a701ca63255a9701d82a28749d98789a7dd
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-07-15 23:54:03 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,19 +1,6 @@
 function solution(nums) {
-    let result = 0
-    const canGet = nums.length/2 
-    const objs = {}
-    nums.forEach((value)=> {
-        if(objs[value]) {
-            objs[value]+=1
-        } else {
-            objs[value] = 1
-        }
-    })
+    const max = nums.length/2
     
-    const typeCnt = Object.keys(objs).length;
-    
-    if(typeCnt >= canGet){
-       return canGet
-    } 
-    return typeCnt
+    const set = new Set(nums)
+    return max <= set.size ? max : set.size
 }
