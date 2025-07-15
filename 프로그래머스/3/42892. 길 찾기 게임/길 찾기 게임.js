cc077b9503d91a27d8c65c25d8c6d20eb947b401
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-07-15 20:35:33 +0900

    [level 3] Title: 길 찾기 게임, Time: 64.69 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
@@ -0,0 +1,107 @@
+class Node {
+    data = null
+    left = null
+    right = null
+    constructor(data) {
+        this.data = data
+    }
+    
+    isLeft() {
+        return this.left === null ? false : true
+    }
+    
+    isRight() {
+        return this.right === null ? false : true
+    }
+}
+
+// 결국 좌측 하위의 하위 노드는, 최상위의 x보다는 작거나 같을수밖에 없음
+function solution(nodeinfo) {
+    const newNodeInfo = nodeinfo
+        .map((value,index) => {return [...value, index+1]})
+        .sort((a,b)=> { return b[1] - a[1] })
+    
+
+    
+    const map = new Map()
+    newNodeInfo.forEach((value)=> {
+        const node = new Node({
+            x: value[0],
+            y: value[1],
+            value: value[2]                  
+        })
+        if(map.get(value[1])) {
+            map.set(value[1], [...map.get(value[1]), node])
+        } else {
+            map.set(value[1], [node])
+        }
+    })
+    
+    let nodeList = []
+    Array.from(map).forEach(([key,value])=> {
+        value.sort((a,b)=> {
+            return a.data.x - b.data.x
+        })
+        nodeList = [...nodeList, ... value]
+    })
+    console.log(nodeList)
+    
+    let root = null
+    for(const node of nodeList) {
+        if(root === null) {
+            root = node
+        } else {
+            let parent = root
+            while(true) {
+                if(node.data.x <= parent.data.x) {      // 내 x좌표가 부모보다 작거나 같은경우
+                    if(parent.isLeft()) {               // 왼쪽에 넣을수 있으면 넣어둠
+                        parent = parent.left
+                    } else {
+                        parent.left = node
+                        break
+                    }
+                    
+                }
+                else {                                  // 부모 오른쪽이 없는데, 내 x좌표가 부모보다 큰경우
+                    if(parent.isRight()) {
+                        parent = parent.right
+                    } else {
+                        parent.right = node
+                        break
+                    }
+                } 
+            }
+        }
+    }
+    const result = []
+    result.push(preOrder(root, []))
+    result.push(postOrder(root, []))
+    return result
+    
+}
+
+const preOrder = (node, result) => {
+    if(node) {
+        result.push(node.data.value)
+        if(node.isLeft()) {
+            preOrder(node.left, result)
+        }
+        if(node.isRight()) {
+            preOrder(node.right, result)
+        }
+        return result
+    }
+}
+
+const postOrder = (node, result) => {
+    if(node) {
+        if(node.isLeft()){
+            postOrder(node.left, result)
+        }
+        if(node.isRight()) {
+            postOrder(node.right, result)
+        }
+        result.push(node.data.value)
+        return result
+    }
+}
