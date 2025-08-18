e359bb7bc6f2192ae8e9a53cef9ae51ee75e81b6
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-08-18 23:52:35 +0900

    [Silver III] Title: N과 M (3), Time: 716 ms, Memory: 151092 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+const input = fs.readFileSync(filePath, 'utf-8')
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number)
+
+
+const N = input[0]
+const M = input[1]
+const output = []
+
+function solution() {
+    const result = []
+    bt(0)
+    function bt(count) {
+        if(result.length === M) {
+            output.push(result.join(" "))
+            return
+        }
+        for(let i=0; i<N; i++) {
+            result.push(i+1)
+            bt(i+1)
+            result.pop()
+        }
+    }
+}
+solution()
+console.log(output.join("\n"))
