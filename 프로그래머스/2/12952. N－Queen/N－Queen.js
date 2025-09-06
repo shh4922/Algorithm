2da5e9b7205629dbd094c53d127438ffda5fd146
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-09-06 20:40:47 +0900

    [level 2] Title: N-Queen, Time: 175.75 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,37 +1,66 @@
 // 격국 한 row 당 한개의 퀸이 있어야함.
-function solution(n) {
-    let result = 0
-    const queenRowList = []
+// function solution(n) {
+//     let result = 0
+//     const queenRowList = []
     
-    function bt(col, qCount) {
-        if(qCount === n) {
-            result+=1
+//     function bt(col, qCount) {
+//         if(qCount === n) {
+//             result+=1
             
-        }
+//         }
         
-        // col -> 세로,  i => 가로
-        for(let i=0; i<n; i++) {
-            if(!isVaildate(queenRowList, col, i)) continue
+//         // col -> 세로,  i => 가로
+//         for(let i=0; i<n; i++) {
+//             if(!isVaildate(queenRowList, col, i)) continue
             
-            queenRowList.push(i)
-            bt(col+1, qCount+1)
-            queenRowList.pop()
+//             queenRowList.push(i)
+//             bt(col+1, qCount+1)
+//             queenRowList.pop()
+//         }
+//     }
+    
+//     bt(0,0)
+//     return result
+// }
+
+
+// // queenRowPositionList [1,3] => (0,1), (1,3) 퀸 있는 상태인거임
+// // col은 짜피 bt호출할떄 한칸씩 올려서 호출하고, row는 bt내에서 i<n 으로 돌리는 상황
+// function isVaildate(queenList, col, row) {
+    
+//     for(let c=0; c<col; c++) {
+//         const r = queenList[c]
+//         if(r === row) { return false }
+//         if(Math.abs(r-row) === Math.abs(c-col)) { return false}
+//     }
+//     return true
+// }
+
+function solution(n) {
+    const queenList = []
+    let result = 0
+    function bt() {
+        if(queenList.length === n) {
+            result++
+            return 
+        }
+        for(let i=0; i<n; i++) { // col
+            if(isValidate(i)) {
+                queenList.push(i)
+                bt()
+                queenList.pop()
+            }
         }
     }
     
-    bt(0,0)
-    return result
-}
-
-
-// queenRowPositionList [1,3] => (0,1), (1,3) 퀸 있는 상태인거임
-// col은 짜피 bt호출할떄 한칸씩 올려서 호출하고, row는 bt내에서 i<n 으로 돌리는 상황
-function isVaildate(queenList, col, row) {
-    
-    for(let c=0; c<col; c++) {
-        const r = queenList[c]
-        if(r === row) { return false }
-        if(Math.abs(r-row) === Math.abs(c-col)) { return false}
+    function isValidate( row) {
+        for(let c=0; c<queenList.length; c++) {
+            const r = queenList[c]
+            if(r === row) return false
+            if(Math.abs(r-row) === Math.abs(c-queenList.length)) return false
+        }
+        return true
     }
-    return true
+    bt()
+    return result
 }
