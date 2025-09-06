79ac7f6f93c7253c9e5a5dc1bb05705d65c07729
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-09-06 20:18:12 +0900

    [level 2] Title: 피로도, Time: 31.26 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -2,25 +2,44 @@
 아무리 봐도 백트래킹인데.... 
 하 제일 쉬운건데 어케풀어야할지 감이 안온다
 */
-function solution(k, dungeons) {
+// function solution(k, dungeons) {
     
-    let hp = k
-    const visited = Array.from({length: dungeons.length}).fill(false)
+//     let hp = k
+//     const visited = Array.from({length: dungeons.length}).fill(false)
     
-    return bt(k, dungeons, 0, visited)
-    
-    
-    
-}
+//     return bt(k, dungeons, 0, visited)
+        
+// }
 
-function bt(curK, dungeons, cnt, visited) {
-    let curCount = cnt
-    for(let i=0; i<dungeons.length; i++) {
-        if(!visited[i] && curK >= dungeons[i][0]) {
-            visited[i] = true
-            curCount = Math.max(curCount, bt(curK-dungeons[i][1], dungeons, cnt+1, visited))
-            visited[i] = false
+// function bt(curK, dungeons, cnt, visited) {
+//     let curCount = cnt
+//     for(let i=0; i<dungeons.length; i++) {
+//         if(!visited[i] && curK >= dungeons[i][0]) {
+//             visited[i] = true
+//             curCount = Math.max(curCount, bt(curK-dungeons[i][1], dungeons, cnt+1, visited))
+//             visited[i] = false
+//         }
+//     }
+//     return curCount
+// }
+
+
+function solution(k, dungeons) {
+    let curHp = k
+    const visited = Array.from({length:dungeons.length}).fill(false)
+    
+    function bt(hp, visited, cnt) {
+        
+        let maxCount = cnt
+        
+        for(let i=0; i<dungeons.length; i++) {
+            if(!visited[i] && hp >= dungeons[i][0]) {
+                visited[i] = true
+                maxCount = Math.max(maxCount, bt(hp-dungeons[i][1], visited, cnt+1))
+                visited[i] = false
+            }
         }
+        return maxCount
     }
-    return curCount
+    return bt(curHp,visited,0)
 }
